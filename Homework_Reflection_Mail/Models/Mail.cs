a89987e2ae3519d5dc73c8267ef2084dabff7671
Zulfuqar Asgerzade <37780533+ZulfuqarAsgerzade@users.noreply.github.com>
2022-12-13 19:16:20 +0400

    Homework done

---
+++ Homework_Reflection_Mail/Models/Mail.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_Reflection_Mail.Models
+{
+    public class Mail
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+
+        public string MailCreator(string firstname, string lastname) {
+            
+            this.Username = (firstname + "." + lastname).ToLower();
+            this.Email = this.Username + " @mail.com";
+
+            return this.Email;
+        }
+
+    }
+}
---
+++ Homework_Reflection_Mail/Program.cs
@@ -0,0 +1,41 @@
+﻿using Homework_Reflection_Mail.Models;
+using System.Text.RegularExpressions;
+
+public class Program
+{
+    public static void Main()
+    {
+
+        Console.Write("Please enter your name: ");
+        string userFrstName = Console.ReadLine();
+
+        Console.Write("Please enter your lastname: ");
+        string userLstName = Console.ReadLine();
+
+
+        Mail mail = new Mail();
+
+        // MethodInfo ile de getirile biler. GetMethod() istifade ederek
+        // MethodInfo daha oxunaqli, amma rahat istifade ucun bu uygun geldi mene
+
+        mail.GetType().GetMethods().ToList().ForEach(method =>
+        {
+            if (method.Name.Equals("MailCreator"))
+            {
+                string userMail = method.Invoke(mail, new object[] { 
+                    Regex.Replace(userFrstName, @"[^0-9a-zA-Z]+", ""), Regex.Replace(userLstName, @"[^0-9a-zA-Z]+", "")
+                }).ToString();
+                /*Console.WriteLine(userMail);*/
+            }
+        });
+
+
+        // Properties
+        mail.GetType().GetProperties().ToList().ForEach(property => {
+
+            Console.WriteLine(property.Name + ": " + property.GetValue(mail));
+        });
+
+
+    }
+}
